0f7f764d56c65c80fdbe7b0871a2f23f1d1e72f5
Nysero <47828516+Nysero@users.noreply.github.com>
2020-03-25 13:30:16 +0000

    Add files via upload

---
+++ src/Connection.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.io.IOException;
+import java.net.Socket;
+
+
+public class Connection implements Runnable {
+
+    private final Socket clientsocket;
+    private final String remoteIp;
+    private final int remotePort;
+    private Socket serverConnection = null;
+
+    public Connection(Socket clientsocket, String remoteIp, int remotePort) {
+        this.clientsocket = clientsocket;
+        this.remoteIp = remoteIp;
+        this.remotePort = remotePort;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("new connection {}:{}" + clientsocket.getInetAddress().getHostName() + clientsocket.getPort());
+        try {
+            serverConnection = new Socket(remoteIp, remotePort);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("Proxy {}:{} <-> {}:{}" + clientsocket.getInetAddress().getHostName() + clientsocket.getPort() + serverConnection.getInetAddress().getHostName() + serverConnection.getPort());
+
+        new Thread(new AnonGW(clientsocket, serverConnection)).start();
+        new Thread(new AnonGW(serverConnection, clientsocket)).start();
+        new Thread(() -> {
+            while (true) {
+                if (clientsocket.isClosed()) {
+                    System.out.println("client socket ({}:{}) closed" + clientsocket.getInetAddress().getHostName() + clientsocket.getPort());
+                    closeServerConnection();
+                    break;
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignored) {}
+            }
+        }).start();
+    }
+
+    private void closeServerConnection() {
+        if (serverConnection != null && !serverConnection.isClosed()) {
+            try {
+                System.out.println("closing remote host connection {}:{}" + serverConnection.getInetAddress().getHostName() + serverConnection.getPort());
+                serverConnection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/TcpIpProxy.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpIpProxy {
+
+    private final String remoteIp;
+    private final int remotePort;
+    private final int port;
+
+    public TcpIpProxy(String remoteIp, int remotePort, int port) {
+        this.remoteIp = remoteIp;
+        this.remotePort = remotePort;
+        this.port = port;
+    }
+
+    public void listen() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("listening...");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                startThread(new Connection(socket, remoteIp, remotePort));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void startThread(Connection connection) {
+        Thread t = new Thread(connection);
+        t.start();
+    }
+}
