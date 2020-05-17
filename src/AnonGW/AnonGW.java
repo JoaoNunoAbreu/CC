550b26e0c43c82a8d0dc28099c9217200ab4d275
João Nuno Abreu <joao.nuno.escola@gmail.com>
2020-05-17 16:51:57 +0100

    ver se os peers estão a ser bem retirados

--- src/AnonGW/AnonGW.java
+++ src/AnonGW/AnonGW.java
@@ -7,7 +7,7 @@ public class AnonGW {
     public static void main(String args[]) {
 
         /**
-         * Exemplo: anonGW target-server 10.3.3.1 remoteport 80 overlay-peers 10.1.1.2 10.4.4.2 10.4.4.3
+         * Exemplo Geral: anonGW target-server 10.3.3.1 remoteport 80 overlay-peers 10.1.1.2 10.4.4.2 10.4.4.3
          */
         if(args.length < 6){
             System.out.println("Número de argumentos inválidos!");
@@ -23,8 +23,16 @@ public class AnonGW {
             }
             System.out.println(Arrays.toString(peers));
 
+            /**
+             * P1 (10.1.1.1) -> Atena (10.4.4.3) -> Serv1 (10.3.3.1)
+             * anonGW target-server 10.3.3.1 remoteport 80 overlay-peers 10.4.4.3
+             *
+             * P1 (10.1.1.1) -> Atena (10.4.4.3) -> Hermes (10.4.4.1) -> Serv1 (10.3.3.1)
+             * anonGW target-server 10.3.3.1 remoteport 80 overlay-peers 10.4.4.3 10.4.4.1
+             */
             new Thread(new AnonGWWorker(remoteHost, remotePort, peers, 12345, 6666)).start();
-            new Thread(new AnonGWWorker(remoteHost, remotePort, peers, 12346, 6666)).start();
+            new Thread(new AnonGWWorker(remoteHost, remotePort, peers, 12346, 6667)).start();
+
         }
     }
 }
--- src/AnonGW/AnonGWWorker.java
+++ src/AnonGW/AnonGWWorker.java
@@ -4,21 +4,30 @@ import Tcp.TcpReceiver;
 import Udp.UdpReceiver;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
+import java.util.Arrays;
 
 public class AnonGWWorker implements Runnable{
 
     private final String remoteIp;
     private final int remotePort;
-    private int[] peers;
+    private int[] nextPeers;
     private int tcpPort;
     private int udpPort;
 
     public AnonGWWorker(String remoteIp, int remotePort, int[] p,int tcpPort, int udpPort) {
         this.remoteIp = remoteIp;
         this.remotePort = remotePort;
-        this.peers = p;
+        this.nextPeers = removeElements(p,udpPort);
         this.tcpPort = tcpPort;
         this.udpPort = udpPort;
+    }
+
+    public static int[] removeElements(int[] arr, int key) {
+        int index = 0;
+        for (int i=0; i<arr.length; i++)
+            if (arr[i] != key)
+                arr[index++] = arr[i];
+        return Arrays.copyOf(arr, index);
     }
 
     /**
@@ -29,7 +38,10 @@ public class AnonGWWorker implements Runnable{
             ServerSocket ss = new ServerSocket(tcpPort);
             DatagramSocket socket_udp = new DatagramSocket(udpPort);
             System.out.println("listening...");
-            new Thread(new TcpReceiver(ss,peers)).start();
+            /**
+             * Falta aqui garantir que tiramos o peer que representa esta instância do AnonGW
+             */
+            new Thread(new TcpReceiver(ss,nextPeers)).start();
             new Thread(new UdpReceiver(socket_udp, remoteIp, remotePort)).start();
         } catch (Exception e) {
             e.printStackTrace();
