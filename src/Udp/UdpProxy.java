60a341e978a1ef1d3090807b45e3570a350ec4e8
João Nuno Abreu <joao.nuno.escola@gmail.com>
2020-05-28 01:01:08 +0100

    testes3

--- src/Udp/UdpProxy.java
+++ src/Udp/UdpProxy.java
@@ -61,6 +61,8 @@ public class UdpProxy implements Runnable {
                 }
             }
 
+            System.out.println("Establecida ligação " + tcp_final.toString());
+
             /* Ligação TCP */
             InputStream br = tcp_final.getInputStream();
             OutputStream pw = tcp_final.getOutputStream();
